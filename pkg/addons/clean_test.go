3baf17e6477f6ecd62f518770c32b18ca4ba9e37
Mangirdas <m.judeikis@gmail.com>
2018-07-26 17:45:56 +0100

    add clean.go test

--- pkg/addons/clean_test.go
+++ pkg/addons/clean_test.go
@@ -1,77 +1,29 @@
 package addons
 
 import (
+	"reflect"
+	"testing"
+
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
-var obj *unstructured.Unstructured
-
-func newTestObj() unstructured.Unstructured {
-
-	trueVar := true
-	ten := int64(10)
-	obj := unstructured.Unstructured{
-		Object: map[string]interface{}{
-			"kind":       "test_kind",
-			"apiVersion": "test_version",
-			"metadata": map[string]interface{}{
-				// keep fields
-				"generateName": "test_generateName",
-				"labels": map[string]interface{}{
-					"test_label": "test_value",
-				},
-				// clean fields
-				"resourceVersion":            "test_resourceVersion",
-				"uid":                        "test_uid",
-				"generation":                 ten,
-				"deletionGracePeriodSeconds": ten,
-				"selfLink":                   "test_selfLink",
-				"creationTimestamp":          "2009-11-10T23:00:00Z",
-				"deletionTimestamp":          "2010-11-10T23:00:00Z",
-				"annotations": map[string]interface{}{
-					// keep annotation
-					"test_annotation": "test_value",
-					// clean annotations
-					"kubectl.kubernetes.io/last-applied-configuration": "test",
-					"openshift.io/generated-by":                        "test_generator",
-				},
-				"ownerReferences": []interface{}{
-					map[string]interface{}{
-						"kind":       "Pod",
-						"name":       "poda",
-						"apiVersion": "v1",
-						"uid":        "1",
-					},
-				},
-			},
-			"spec": map[string]interface{}{
-				"initContainers": []interface{}{
-					map[string]interface{}{
-						"name":            "init1",
-						"image":           "image1",
-						"imagePullPolicy": "Always",
-					},
-					map[string]interface{}{
-						"name":            "init2",
-						"image":           "image2",
-						"imagePullPolicy": "Always",
-					},
-				},
-				"containers": []interface{}{
-					map[string]interface{}{
-						"name":            "init1",
-						"image":           "image1",
-						"imagePullPolicy": "Always",
-					},
-					map[string]interface{}{
-						"name":            "init2",
-						"image":           "image2",
-						"imagePullPolicy": "Always",
-					},
-				},
-			},
+func TestClean(t *testing.T) {
+	tests := []struct {
+		name    string
+		testNum int
+		input   *unstructured.Unstructured
+		want    *unstructured.Unstructured
+	}{
+		{
+			name:    "Status clean",
+			testNum, 1
 		},
 	}
 
-	return obj
+	for _, tt := range tests {
+
+		_, filename, _, ok := runtime.Caller(1)
+		log.Print(filename)
+	}
+
 }
